497e073c34a34393a6867dc51fe78bc2f433de63
chen var <varsanochen@gmail.com>
2022-05-11 16:43:59 +0300

    ffa

--- src/main/java/com/example/reversed/config/TasksConfiguration.java
+++ src/main/java/com/example/reversed/config/TasksConfiguration.java
@@ -5,11 +5,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableScheduling
-@EnableSwagger2
 @EnableAspectJAutoProxy
 public class TasksConfiguration {
 }
